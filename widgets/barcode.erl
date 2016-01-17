17bad1c755150a71815f748781348e75d57b09ac
skvamme <skvammedev@gmail.com>
2016-01-17 13:52:05 +0100

    new widgets

--- widgets/barcode.erl
+++ widgets/barcode.erl
@@ -3,6 +3,7 @@
 -export([make/5,init/5,loop/4]).
 -define (WT,300).
 -define (HT,200).
+-define (SPACE,2).
 -include("ex11_lib.hrl").
 -import(ex11_lib, [xDo/2,xPen/3,xClearArea/1,xFlush/1,xColor/2,eFillPoly/5,xCreateSimpleWindow/10,eMapWindow/1,mkPoint/2,xSetScreenSaver/2]).
 
@@ -57,10 +58,10 @@ d_n(Display,Window,Black,[Head|Tail],X) ->
 d_n_n(_,_,_,[],X) -> X;
 d_n_n(Display,Window,Black,[Head|Tail],X) ->
 	X1 = case Head of
-		0 -> xDo(Display,eFillPoly(Window,Black,convex,origin,[mkPoint(X,0),mkPoint(X+3,0),mkPoint(X+3,200),mkPoint(X,200)])), 2;
-		1 -> xDo(Display,eFillPoly(Window,Black,convex,origin,[mkPoint(X,0),mkPoint(X+9,0),mkPoint(X+9,200),mkPoint(X,200)])), 6;
-		2 -> 2;
-		3 -> 6
+		0 -> xDo(Display,eFillPoly(Window,Black,convex,origin,[mkPoint(X,0),mkPoint(X + ?SPACE,0),mkPoint(X + ?SPACE,?HT),mkPoint(X,?HT)])), ?SPACE;
+		1 -> xDo(Display,eFillPoly(Window,Black,convex,origin,[mkPoint(X,0),mkPoint(X+(3 * ?SPACE),0),mkPoint(X+(3 * ?SPACE),?HT),mkPoint(X,?HT)])), 3 * ?SPACE;
+		2 -> ?SPACE;
+		3 -> 3 * ?SPACE
 	end,
 	d_n_n(Display,Window,Black,Tail,X+X1).
 
