708794dc853da889fecc5d3fd32e6f4f0f4898ef
Yoshiki Ohshima <Yoshiki.Ohshima@acm.org>
2016-03-28 16:34:54 -0700

    does not clear the area.

--- morphic/morph.erl
+++ morphic/morph.erl
@@ -1,45 +1,31 @@
 -module(morph).
 -author(ohshima).
--export([newMorph/2]).
+-export([newMorph/4]).
 -include("ex11_lib.hrl").
 -import(ex11_lib, [xDo/2, xFlush/1,rpc/2,xCreateGC/2,xColor/2,mkRectangle/4,xCreateSimpleWindow/7,
     eMapWindow/1,xCreateWindow/10,xCreateCursor/2,ePolyFillRectangle/3,xCreatePixmap/4,eCopyArea/9,ePolyArc/3,ePolyFillArc/3,
     mkArc/6,mkPoint/2,ePolyLine/4,xPen/3,xSetScreenSaver/2]).
 
-newMorph(Display, Parent) ->
- init(Display, Parent).
+newMorph(Display, Win, X, Y) ->
+ init(Display, Win, X, Y).
 
-init(Display, Parent) ->
+init(Display, Win, X, Y) ->
   Width = 50,
   Height = 40,
-  X = 100,
-  Y = 100,
   Color = xCreateGC(Display, [{function,'copy'},{line_width,5},{arc_mode,chord},{line_style,solid},
-                {graphics_exposures, false},{foreground, xColor(Display, ?black)}]),
-  Win = xCreateWindow(Display, Parent, X, Y, Width, Height, 0, 0, 0,
-		  [{backgroundPixel, Color},
-		   {eventMask, 
-		    ?EVENT_EXPOSURE bor 
-		    ?EVENT_STRUCTURE_NOTIFY bor 
-		    ?EVENT_BUTTON_PRESS bor 
-                    %?EVENT_BUTTON1_MOTION bor 
-		    ?EVENT_BUTTON_RELEASE},
-		   {backgroundPixmap, 0}, 
-		   {cursor, xCreateCursor(Display,?XC_arrow)},
-		   {borderPixmap,0}]),
-  xDo(Display, eMapWindow(Win)),
+                {graphics_exposures, false},{foreground, xColor(Display, ?red)}]),
+  morph_loop(Display, Win, Width, Height, X, Y, Color).
 
-  morph_loop(Display, Win, Parent, Width, Height, X, Y, Color).
-
-morph_loop(Display, Win, Parent, Width, Height, X, Y, Color) ->
+morph_loop(Display, Win, Width, Height, X, Y, Color) ->
   receive
     Msg ->
       io:format("~p got msg: ~p~n",[?MODULE, Msg])
   end,
   case Msg of 
-    {'event',_,'buttonPress',_} -> 
-      NewX = X + 1;
-    {'morph_draw'} -> 
+    {buttonPress} -> 
+      io:format("morph buttonPress~n"),
+      NewX = X + 5;
+    {morph_draw} ->
       NewX = X,
       io:format("morph_draw~n"),
       draw(Display, Win, Width, Height, NewX, Y, Color);
@@ -47,9 +33,8 @@ morph_loop(Display, Win, Parent, Width, Height, X, Y, Color) ->
       NewX = X,
       true
   end,
-  morph_loop(Display, Win, Parent, Width, Height, NewX, Y, Color).
+  morph_loop(Display, Win, Width, Height, NewX, Y, Color).
 
 draw(Display, Win, Width, Height, X, Y, Color) -> 
   Rect = mkRectangle(X, Y, Width, Height),
-  io:format("Morph: ~p, ~p, ~p, ~p~n",[Display, Win, Rect, Color]),
   xDo(Display, ePolyFillRectangle(Win, Color, [Rect])).
--- morphic/morphic.erl
+++ morphic/morphic.erl
@@ -3,10 +3,10 @@
 -export([start/0,init/0]).
 -include("ex11_lib.hrl").
 -import(ex11_lib, [xDo/2, xFlush/1,rpc/2,xCreateGC/2,xColor/2,mkRectangle/4,xCreateSimpleWindow/7,
-    eMapWindow/1,ePolyFillRectangle/3,xCreatePixmap/4,eCopyArea/9,ePolyArc/3,ePolyFillArc/3,
+    eMapWindow/1,ePolyFillRectangle/3,xCreatePixmap/4,
     mkArc/6,mkPoint/2,ePolyLine/4,xPen/3,xSetScreenSaver/2]).
 
--import(morph, [newMorph/2, test/0]).
+-import(morph, [newMorph/4]).
 
 start() -> spawn(?MODULE,init,[]).
 
@@ -16,15 +16,15 @@ init() ->
   Win = xCreateSimpleWindow(Display,400,0,400,400,?XC_arrow,xColor(Display,?black)),
   xDo(Display, eMapWindow(Win)),
   xFlush(Display),
-  M = spawn(morph, newMorph, [Display,Win]),
-  io:format("morph: ~p, ~p~n", [Display, M]),
-  Scene = [M],
+  M = spawn(morph, newMorph, [Display, Win, 300, 300]),
+  N = spawn(morph, newMorph, [Display, Win, 250, 250]),
+  Scene = [M, N],
   loop(Display, Win, Scene).
 
 loop(Display, Win, Scene) ->
   receive
-    Msg ->
-      io:format("~p got msg: ~p~n",[?MODULE, Msg])
+    {event, _, buttonPress, _} ->
+      lists:nth(1, Scene) ! {'buttonPress'}
   after 1000 -> 
     io:format("after ~p~n", [Scene]),
     draw(Display, Win, Scene)
@@ -32,12 +32,11 @@ loop(Display, Win, Scene) ->
   loop(Display, Win, Scene).
 
 draw(Display, Win, Scene) ->
-    White = xCreateGC(Display, [{function,'copy'},{line_width,5},{arc_mode,chord},{line_style,solid},
- 	 {graphics_exposures, false},{foreground, xColor(Display, ?white)}]),
+    Black = xCreateGC(Display, [{function,'set'},{line_width,5},{arc_mode,chord},{line_style,solid},
+ 	 {graphics_exposures, true},{foreground, xColor(Display, ?white)}]),
 
-  Rect = mkRectangle(0, 0, 200, 200),
-  io:format("Top: ~p, ~p, ~p~n",[Display, Rect, White]),
-  xDo(Display, ePolyFillRectangle(Win, White, [Rect])),
+  Rect = mkRectangle(0, 0, 400, 200),
+  xDo(Display, ePolyFillRectangle(Win, Black, [Rect])),
   xFlush(Display),
   lists:map(fun(M) -> 
     M ! {'morph_draw'} end,
