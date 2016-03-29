90f45cf5e715ecbbb1c9d980f427719284bc3700
Yoshiki Ohshima <Yoshiki.Ohshima@acm.org>
2016-03-29 13:59:46 -0700

    okay, at least I figured out a few things.

--- morphic/morph.erl
+++ morphic/morph.erl
@@ -6,17 +6,17 @@
     eMapWindow/1,xCreateWindow/10,xCreateCursor/2,ePolyFillRectangle/3,xCreatePixmap/4,eCopyArea/9,ePolyArc/3,ePolyFillArc/3,
     mkArc/6,mkPoint/2,ePolyLine/4,xPen/3,xSetScreenSaver/2]).
 
-newMorph(Display, Win, X, Y) ->
- init(Display, Win, X, Y).
+newMorph(Display, Pix, X, Y) ->
+ init(Display, Pix, X, Y).
 
-init(Display, Win, X, Y) ->
+init(Display, Pix, X, Y) ->
   Width = 50,
   Height = 40,
   Color = xCreateGC(Display, [{function,'copy'},{line_width,5},{arc_mode,chord},{line_style,solid},
                 {graphics_exposures, false},{foreground, xColor(Display, ?red)}]),
-  morph_loop(Display, Win, Width, Height, X, Y, Color).
+  morph_loop(Display, Pix, Width, Height, X, Y, Color).
 
-morph_loop(Display, Win, Width, Height, X, Y, Color) ->
+morph_loop(Display, Pix, Width, Height, X, Y, Color) ->
   receive
     Msg ->
       io:format("~p got msg: ~p~n",[?MODULE, Msg])
@@ -28,13 +28,13 @@ morph_loop(Display, Win, Width, Height, X, Y, Color) ->
     {morph_draw} ->
       NewX = X,
       io:format("morph_draw~n"),
-      draw(Display, Win, Width, Height, NewX, Y, Color);
+      draw(Display, Pix, Width, Height, NewX, Y, Color);
     _ -> 
       NewX = X,
       true
   end,
-  morph_loop(Display, Win, Width, Height, NewX, Y, Color).
+  morph_loop(Display, Pix, Width, Height, NewX, Y, Color).
 
-draw(Display, Win, Width, Height, X, Y, Color) -> 
+draw(Display, Pix, Width, Height, X, Y, Color) -> 
   Rect = mkRectangle(X, Y, Width, Height),
-  xDo(Display, ePolyFillRectangle(Win, Color, [Rect])).
+  xDo(Display, ePolyFillRectangle(Pix, Color, [Rect])).
--- morphic/morphic.erl
+++ morphic/morphic.erl
@@ -3,7 +3,8 @@
 -export([start/0,init/0]).
 -include("ex11_lib.hrl").
 -import(ex11_lib, [xDo/2, xFlush/1,rpc/2,xCreateGC/2,xColor/2,mkRectangle/4,xCreateSimpleWindow/7,
-    eMapWindow/1,ePolyFillRectangle/3,xCreatePixmap/4,
+    eMapWindow/1,ePolyFillRectangle/3,xCreatePixmap/4,	 eCopyArea/9,
+    ePutImage/9,
     mkArc/6,mkPoint/2,ePolyLine/4,xPen/3,xSetScreenSaver/2]).
 
 -import(morph, [newMorph/4]).
@@ -14,31 +15,39 @@ init() ->
   {ok, Display} = ex11_lib:xStart("3.1"),
   xSetScreenSaver(Display,0),
   Win = xCreateSimpleWindow(Display,400,0,400,400,?XC_arrow,xColor(Display,?black)),
+  Pix = xCreatePixmap(Display, Win, 400, 400),
   xDo(Display, eMapWindow(Win)),
   xFlush(Display),
-  M = spawn(morph, newMorph, [Display, Win, 300, 300]),
-  N = spawn(morph, newMorph, [Display, Win, 250, 250]),
+  M = spawn(morph, newMorph, [Display, Pix, 300, 300]),
+  N = spawn(morph, newMorph, [Display, Pix, 250, 250]),
   Scene = [M, N],
-  loop(Display, Win, Scene).
+  loop(Display, Win, Scene, Pix).
 
-loop(Display, Win, Scene) ->
+loop(Display, Win, Scene, Pix) ->
   receive
     {event, _, buttonPress, _} ->
       lists:nth(1, Scene) ! {'buttonPress'}
   after 1000 -> 
     io:format("after ~p~n", [Scene]),
-    draw(Display, Win, Scene)
+    draw(Display, Win, Scene, Pix)
   end,
-  loop(Display, Win, Scene).
+  loop(Display, Win, Scene, Pix).
 
-draw(Display, Win, Scene) ->
-    Black = xCreateGC(Display, [{function,'set'},{line_width,5},{arc_mode,chord},{line_style,solid},
- 	 {graphics_exposures, true},{foreground, xColor(Display, ?white)}]),
+draw(Display, Win, Scene, Pix) ->
+    Black = xCreateGC(Display, [{function,'copy'},{line_width,5},{arc_mode,chord},{line_style,solid},
+ 	 {graphics_exposures, true},{foreground, xColor(Display, ?black)}]),
 
-  Rect = mkRectangle(0, 0, 400, 200),
-  xDo(Display, ePolyFillRectangle(Win, Black, [Rect])),
+  Rect = mkRectangle(0, 0, 400, 400),
+  xDo(Display, ePolyFillRectangle(Pix, Black, [Rect])),
   xFlush(Display),
   lists:map(fun(M) -> 
     M ! {'morph_draw'} end,
     Scene),
+
+  Copy = xCreateGC(Display, [{function,'copy'},{line_width,5},{arc_mode,chord},{line_style,solid},
+ 	 {graphics_exposures, true}, {foreground, xColor(Display, ?white)}]),
+
+  xDo(Display, eCopyArea(Pix, Win, Copy, 0, 0, 0, 0, 400, 400)),
+%  ePutImage(Win, Copy, 400, 400, 0, 0, 0, 32, Pix),
   xFlush(Display).
+
