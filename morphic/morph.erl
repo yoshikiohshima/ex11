fcac99c51a85c63e03f533a67db4d2d9c47cb6dc
Yoshiki Ohshima <Yoshiki.Ohshima@acm.org>
2016-03-30 16:13:15 -0700

    refactoring with records.

--- morphic/morph.erl
+++ morphic/morph.erl
@@ -2,85 +2,72 @@
 -author(ohshima).
 -export([newMorph/4]).
 -include("ex11_lib.hrl").
--import(ex11_lib, [xDo/2, xFlush/1,rpc/2,xCreateGC/2,xColor/2,mkRectangle/4,xCreateSimpleWindow/7,
-    eMapWindow/1,xCreateWindow/10,xCreateCursor/2,ePolyFillRectangle/3,xCreatePixmap/4,eCopyArea/9,ePolyArc/3,ePolyFillArc/3,
-    mkArc/6,mkPoint/2,ePolyLine/4,xPen/3,xSetScreenSaver/2]).
+-import(ex11_lib, [xDo/2, xCreateGC/2,xColor/2,mkRectangle/4, ePolyFillRectangle/3]).
 
 -define(containsPoint(X, Y, W, H, MX, MY),
         (X =< MX) andalso (MX < (X + W)) andalso (Y =< MY) andalso (MY < (Y + H))).
 
--define(has(K, M),
-        case (catch maps:get(K, M)) of
-           {'EXIT', _} -> false;
-            _ -> true
-        end).
+-record(morphData, {x = 0, y = 0, width = 50, height = 40, color}).
+-record(handlerData, {down = 0, up = 0, move = 0}).
 
 newMorph(Display, Pix, X, Y) ->
  init(Display, Pix, X, Y).
 
 init(Display, Pix, X, Y) ->
-  W = 50,
-  H = 40,
   Color = xCreateGC(Display, [{function,'copy'},{line_width,5},{arc_mode,chord},{line_style,solid},
-                {graphics_exposures, false},{foreground, xColor(Display, ?blue)}]),
-  morph_loop(Display, Pix, W, H, X, Y, Color, {0, 0, 0}).   % {down, up, move}.  Values are either zero or a record of {fun, params}.
+                {graphics_exposures, false},{foreground, xColor(Display, 16#1010E0)}]),
+  Data = #morphData{x=X, y=Y, color = Color},
+  morph_loop(Display, Pix, Data, {0, 0, 0}).   % {down, up, move}.  Values are either zero or a record of {fun, params}.
 
-morph_loop(Display, Pix, W, H, X, Y, Color, E) ->
+morph_loop(Display, Pix, Data, E) ->
   {Down, Up, Move} = E,
   receive
     {beDraggable} ->
-      NewX = X,
-      NewY = Y,
       NewE = {{drag, {}}, {drag, {}}, {drag, {}}};
-    {buttonPress, {P, BX, BY, SX, SY}}
-         when ?containsPoint(X, Y, W, H, BX, BY),
+    {'data', NewData} ->
+       NewE = E,
+       morph_loop(Display, Pix, NewData, E);
+    {buttonPress, {P, BX, BY, _, _}}
+         when ?containsPoint(Data#morphData.x, Data#morphData.y, Data#morphData.width, Data#morphData.height, BX, BY),
               Down /= 0 -> 
       {F, Param} = Down,
-      NewDown = down(F, E, {P, BX, BY, SX, SY}, X, Y),
-      NewE = {NewDown, Up, Move},
-      NewX = X,
-      NewY = Y;
-    {buttonMove, {P, BX, BY, SX, SY}}
-         when ?containsPoint(X, Y, W, H, BX, BY),
+      NewDown = down(F, E, {P, BX, BY}, Data),
+      NewE = {NewDown, Up, Move};
+    {buttonMove, {P, BX, BY, _, _}}
+         when ?containsPoint(Data#morphData.x, Data#morphData.y, Data#morphData.width, Data#morphData.height, BX, BY),
               Move /= 0 -> 
       {F, Param} = Move,
-      NewMove = move(F, E, {P, BX, BY, SX, SY}, X, Y),
-      {_, {NewX, NewY}} = NewMove,
+      NewMove = move(F, E, {P, BX, BY}, Data),
       NewE = {Down, Up, NewMove};
-    {buttonUp, {P, BX, BY, SX, SY}}
-         when ?containsPoint(X, Y, W, H, BX, BY),
+    {buttonUp, {P, BX, BY, _, _}}
+         when ?containsPoint(Data#morphData.x, Data#morphData.y, Data#morphData.width, Data#morphData.height, BX, BY),
               Up /= 0 -> 
       {F, Param} = Up,
-      NewUp = up(F, E, {P, BX, BY, SX, SY}, X, Y),
-      NewX = X,
-      NewY = Y,
+      NewUp = up(F, E, {P, BX, BY}, Data),
       NewE = {Down, NewUp, Move};
     {morph_draw} ->
       NewE = E,
-      NewX = X,
-      NewY = Y,
-      draw(Display, Pix, W, H, X, Y, Color);
+      draw(Display, Pix, Data);
     _ -> 
-      NewE = E,
-      NewX = X,
-      NewY = Y
+      NewE = E
   end,
-  morph_loop(Display, Pix, W, H, NewX, NewY, Color, NewE).
+  morph_loop(Display, Pix, Data, NewE).
 
-down(drag, E, EV, X, Y) ->
-  {P, BX, BY, SX, SY} = EV,
-  {drag, {BX - X, BY - Y}}.
+down(drag, E, EV, Data) ->
+  {P, BX, BY} = EV,
+  {drag, {BX - Data#morphData.x, BY - Data#morphData.y}}.
 
-move(drag, E, EV, X, Y) ->
-  {P, BX, BY, SX, SY} = EV,
+move(drag, E, EV, Data) ->
+  {P, BX, BY} = EV,
   {{_, {OrigXDiff, OrigYDiff}}, _, _} = E,
-  {drag, {BX - OrigXDiff, BY - OrigYDiff}}.
+  self() ! {'data', Data#morphData{x = BX - OrigXDiff, y = BY - OrigYDiff}},
+  {drag, {}}.
 
-up(drag, E, EV, X, Y) ->
-  {P, BX, BY, SX, SY} = EV,
+up(drag, E, EV, Data) ->
+%  {P, BX, BY} = EV,
   {{_, {OrigXDiff, OrigYDiff}}, _, _} = E,
-  {drag, 0, 0}.
+  {drag, {}}.
 
-draw(Display, Pix, W, H, X, Y, Color) -> 
-  Rect = mkRectangle(X, Y, W, H),
-  xDo(Display, ePolyFillRectangle(Pix, Color, [Rect])).
+draw(Display, Pix, Data) -> 
+  Rect = mkRectangle(Data#morphData.x, Data#morphData.y, Data#morphData.width, Data#morphData.height),
+  xDo(Display, ePolyFillRectangle(Pix, Data#morphData.color, [Rect])).
