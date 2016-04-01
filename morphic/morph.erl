826477ac43d09918a2a7c6aaefd8307f1a5207c6
Yoshiki Ohshima <Yoshiki.Ohshima@acm.org>
2016-04-01 13:44:16 -0700

    add target function.

--- morphic/morph.erl
+++ morphic/morph.erl
@@ -1,74 +1,73 @@
 -module(morph).
 -author(ohshima).
--export([newMorph/4]).
+-export([newMorph/5]).
 -include("ex11_lib.hrl").
 -import(ex11_lib, [xDo/2, xCreateGC/2,xColor/2,mkRectangle/4, ePolyFillRectangle/3]).
 
 -define(containsPoint(X, Y, W, H, MX, MY),
         (X =< MX) andalso (MX < (X + W)) andalso (Y =< MY) andalso (MY < (Y + H))).
 
--record(morphData, {x = 0, y = 0, width = 50, height = 40, color}).
+-record(data, {x = 0, y = 0, width = 50, height = 40, color}).
 -record(handler, {down = 0, up = 0, move = 0}).
 
-newMorph(Display, Pix, X, Y) ->
- init(Display, Pix, X, Y).
+newMorph(Morphic, Display, Pix, X, Y) -> init(Morphic, Display, Pix, X, Y).
 
-init(Display, Pix, X, Y) ->
-  Color = xCreateGC(Display, [{function,'copy'},{line_width,5},{arc_mode,chord},{line_style,solid},
-                {graphics_exposures, false},{foreground, xColor(Display, 16#1010E0)}]),
-  Data = #morphData{x=X, y=Y, color = Color},
+init(Morphic, Display, Pix, X, Y) ->
+  Color = 16#1010E0,
+  Data = #data{x=X, y=Y, color=Color},
   Handler = #handler{},
-  morph_loop(Display, Pix, Data, Handler).   % {down, up, move}.  Values are either zero or a record of {fun, params}.
+  loop(Morphic, Display, Pix, Data, Handler).   % {down, up, move}.  Values are either zero or a record of {fun, params}.
 
-morph_loop(Display, Pix, Data, Handler) ->
-  io:format("Handler: ~p~n", [Handler#handler.down]),
+loop(Morphic, Display, Pix, Data, Handler) ->
   receive
     {'data', NewData} ->
-       morph_loop(Display, Pix, NewData, Handler);
+       loop(Morphic, Display, Pix, NewData, Handler);
     {'handlers', NewHandler} ->
-      morph_loop(Display, Pix, Data, NewHandler);
+      loop(Morphic, Display, Pix, Data, NewHandler);
     {beDraggable} ->
       self() ! {'handlers', Handler#handler{down={drag, {}}, up={drag, {}}, move={drag, {}}}},
-      morph_loop(Display, Pix, Data, Handler);
+      loop(Morphic, Display, Pix, Data, Handler);
     {buttonPress, {P, BX, BY, _, _}}
-         when ?containsPoint(Data#morphData.x, Data#morphData.y, Data#morphData.width, Data#morphData.height, BX, BY),
+         when ?containsPoint(Data#data.x, Data#data.y, Data#data.width, Data#data.height, BX, BY),
                Handler#handler.down /= 0 -> 
       {F, _} = Handler#handler.down,
       down(F, Handler, {P, BX, BY}, Data),
-      morph_loop(Display, Pix, Data, Handler);
+      loop(Morphic, Display, Pix, Data, Handler);
     {buttonMove, {P, BX, BY, _, _}}
-         when ?containsPoint(Data#morphData.x, Data#morphData.y, Data#morphData.width, Data#morphData.height, BX, BY),
+         when ?containsPoint(Data#data.x, Data#data.y, Data#data.width, Data#data.height, BX, BY),
               Handler#handler.move /= 0 -> 
       {F, _} = Handler#handler.move,
       move(F, Handler, {P, BX, BY}, Data),
-      morph_loop(Display, Pix, Data, Handler);
+      loop(Morphic, Display, Pix, Data, Handler);
     {buttonUp, {P, BX, BY, _, _}}
-         when ?containsPoint(Data#morphData.x, Data#morphData.y, Data#morphData.width, Data#morphData.height, BX, BY),
+         when ?containsPoint(Data#data.x, Data#data.y, Data#data.width, Data#data.height, BX, BY),
               Handler#handler.up /= 0 -> 
       {F, _} = Handler#handler.up,
       up(F, Handler, {P, BX, BY}, Data),
-      morph_loop(Display, Pix, Data, Handler);
-    {morph_draw} ->
-      draw(Display, Pix, Data),
-      morph_loop(Display, Pix, Data, Handler);
-    _ -> morph_loop(Display, Pix, Data, Handler)
+      loop(Morphic, Display, Pix, Data, Handler);
+    {draw} ->
+      Morphic ! {'tell', {self(), Data}},
+      draw(Morphic, Display, Pix, Data),
+      loop(Morphic, Display, Pix, Data, Handler);
+    _ -> loop(Morphic, Display, Pix, Data, Handler)
   end.
-  
 
 down(drag, Handler, EV, Data) ->
-  {P, BX, BY} = EV,
-  NewHandler = Handler#handler{down = {drag, {BX - Data#morphData.x, BY - Data#morphData.y}}},
+  {_, BX, BY} = EV,
+  NewHandler = Handler#handler{down = {drag, {BX - Data#data.x, BY - Data#data.y}}},
   self() ! {'handlers', NewHandler}.
 
 move(drag, Handler, EV, Data) ->
-  {P, BX, BY} = EV,
+  {_, BX, BY} = EV,
   OrigT = Handler#handler.down,
   {_, {OrigXDiff, OrigYDiff}} = OrigT,
-  self() ! {'data', Data#morphData{x = BX - OrigXDiff, y = BY - OrigYDiff}}.
+  self() ! {'data', Data#data{x = BX - OrigXDiff, y = BY - OrigYDiff}}.
 
-up(drag, E, EV, Data) ->
+up(drag, Handler, EV, Data) ->
   true.
 
-draw(Display, Pix, Data) -> 
-  Rect = mkRectangle(Data#morphData.x, Data#morphData.y, Data#morphData.width, Data#morphData.height),
-  xDo(Display, ePolyFillRectangle(Pix, Data#morphData.color, [Rect])).
+draw(Morphic, Display, Pix, Data) -> 
+  Color = xCreateGC(Display, [{function,'copy'},{line_width,5},{arc_mode,chord},{line_style,solid},
+                {graphics_exposures, false},{foreground, xColor(Display, Data#data.color)}]),
+  Rect = mkRectangle(Data#data.x, Data#data.y, Data#data.width, Data#data.height),
+  xDo(Display, ePolyFillRectangle(Pix, Color, [Rect])).
--- morphic/morphic.erl
+++ morphic/morphic.erl
@@ -7,7 +7,11 @@
     ePutImage/9,
     mkArc/6,mkPoint/2,ePolyLine/4,xPen/3,xSetScreenSaver/2]).
 
--import(morph, [newMorph/4]).
+-import(morph, [newMorph/5]).
+-record(data, {x, y, width, height, color}).
+
+-define(containsPoint(X, Y, W, H, MX, MY),
+  (X =< MX) andalso (MX < (X + W)) andalso (Y =< MY) andalso (MY < (Y + H))).
 
 start() -> spawn(?MODULE,init,[]).
 
@@ -18,41 +22,65 @@ init() ->
   Pix = xCreatePixmap(Display, Win, 400, 400),
   xDo(Display, eMapWindow(Win)),
   xFlush(Display),
-  M = spawn(morph, newMorph, [Display, Pix, 300, 300]),
-%  N = spawn(morph, newMorph, [Display, Pix, 250, 250]),
-  Scene = [M],
+  M = spawn(morph, newMorph, [self(), Display, Pix, 300, 300]),
+  N = spawn(morph, newMorph, [self(), Display, Pix, 250, 250]),
+  Scene = [M, N],
+  Props = #{}, % {Pid => data}; created at every frame
   M ! {'beDraggable'},
-  loop(Display, Win, Scene, Pix).
+  loop(Display, Win, Pix, Scene, Props).
 
-loop(Display, Win, Scene, Pix) ->
+loop(Display, Win, Pix, Scene, Props) ->
+  io:format("Props: ~p~n", [Props]),
   receive
+    {'newMorph', Pid} ->
+      loop(Display, Win, Pix, [Pid | Scene], Props);
     {event, _, buttonPress, E} ->
-      lists:nth(1, Scene) ! {'buttonPress', E};
+      lists:nth(1, Scene) ! {'buttonPress', E},
+      loop(Display, Win, Pix, Scene, Props);
     {event, _, motionNotify, E} ->
-      lists:nth(1, Scene) ! {'buttonMove', E};
+      lists:nth(1, Scene) ! {'buttonMove', E},
+      loop(Display, Win, Pix, Scene, Props);
     {event, _, motionRelease, E} ->
-      lists:nth(1, Scene) ! {'buttonUp', E};
+      lists:nth(1, Scene) ! {'buttonUp', E},
+      loop(Display, Win, Pix, Scene, Props);
+    {copyPix} ->
+      copyPix(Display, Win, Pix),
+      loop(Display, Win, Pix, Scene, Props);
+    {'tell', {Pid, Data}} ->
+      loop(Display, Win, Pix, Scene, Props#{Pid=>Data});
     X ->
-      io:format("X: ~p~n", [X])
+      io:format("X: ~p~n", [X]),
+      loop(Display, Win, Pix, Scene, Props)
   after 20 ->
-    draw(Display, Win, Scene, Pix)
-  end,
-  loop(Display, Win, Scene, Pix).
+    draw(Display, Win, Scene, Pix),
+    loop(Display, Win, Pix, Scene, #{})
+  end.
 
 draw(Display, Win, Scene, Pix) ->
-    Back = xCreateGC(Display, [{function,'copy'},{line_width,5},{arc_mode,chord},{line_style,solid},
+  Back = xCreateGC(Display, [{function,'copy'},{line_width,5},{arc_mode,chord},{line_style,solid},
  	 {graphics_exposures, true},{foreground, xColor(Display, ?gray)}]),
 
   Rect = mkRectangle(0, 0, 400, 400),
   xDo(Display, ePolyFillRectangle(Pix, Back, [Rect])),
 %  xFlush(Display),
-  lists:map(fun(M) -> 
-    M ! {'morph_draw'} end,
+  lists:foreach(fun(M) -> 
+    M ! {'draw'} end,
     Scene),
+  self() ! {'copyPix'}.
 
+copyPix(Display, Win, Pix) ->
   Copy = xCreateGC(Display, [{function,'copy'},{line_width,5},{arc_mode,chord},{line_style,solid},
  	 {graphics_exposures, true}, {foreground, xColor(Display, ?white)}]),
 
   xDo(Display, eCopyArea(Pix, Win, Copy, 0, 0, 0, 0, 400, 400)),
   xFlush(Display).
 
+target([], X, Y, Props) -> false;
+target([M|MS], X, Y, Props) ->
+  Prop = (catch maps:get(M, Props)),
+  Val = is_record(Prop, data) andalso ?containsPoint(Prop#data.x, Prop#data.y, Prop#data.width, Prop#data.height, X, Y),
+  io:format("Val: ~p~n", [Val]),
+  case Val of
+    true -> M;
+    _ -> target(MS, X, Y, Props)
+  end.
