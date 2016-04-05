c38dd576b5ce787534bcb1040910a8fef46cab69
Yoshiki Ohshima <Yoshiki.Ohshima@acm.org>
2016-04-04 19:02:20 -0700

    a bit of clean up.

--- morphic/timer.erl
+++ morphic/timer.erl
@@ -2,13 +2,8 @@
 -author(ohshima).
 -export([newTimer/1]).
 
-newTimer(Morphic) -> 
-  loop(Morphic).
+newTimer(Morphic) ->  loop(Morphic).
 
 loop(Morphic) ->
-  receive
-    _ -> true
-  after 30 ->
-    Morphic ! {'display'}
-  end,
+  receive after 30 -> Morphic ! {'display'} end,
   loop(Morphic).
